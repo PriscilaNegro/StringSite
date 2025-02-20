60e325384ce7f81b4b4dde3a1352deb76a048b8e
PriscilaNegro <priscila.luzn@hotmail.com>
2025-02-20 11:06:32 -0300

    Adicionando a página Contatos

--- script.js
+++ script.js
@@ -4,8 +4,7 @@ document.addEventListener("DOMContentLoaded", function() {
     image.classList.add("loaded"); // Adiciona a classe para o efeito de surgimento
 });
 
-//efeito das letras h1 e h2
-function typeWriterEffect(element, text, speed) {
+function typeWriterEffect(element, text, speed) { //efeito das letras h1 e h2
     let i = 0;
     element.innerHTML = "";
     function type() {
@@ -18,11 +17,36 @@ function typeWriterEffect(element, text, speed) {
     type();
   }
   
-  document.addEventListener("DOMContentLoaded", function () {
+  document.addEventListener("DOMContentLoaded", function () {  // Seleciona os elementos para o efeito de digitação
+   
     const title = document.getElementById("title");
     const subtitle = document.getElementById("subtitle");
-  
-    setTimeout(() => typeWriterEffect(title, "ARTE ENTRELAÇADA", 100), 500);
-    setTimeout(() => typeWriterEffect(subtitle, "Ateliê by Jessica Luz", 100), 2000);
-  });
-  
+    const aboutTitle = document.querySelector(".main-title");
+    const contactTitle = document.querySelector(".title");
+
+    // Aplica o efeito de digitação apenas se o elemento existir
+    if (title) {
+        setTimeout(() => typeWriterEffect(title, "ARTE ENTRELAÇADA", 100), 500);
+    }
+    if (subtitle) {
+        setTimeout(() => typeWriterEffect(subtitle, "Ateliê by Jessica Luz", 100), 2000);
+    }
+    if (aboutTitle) {
+        typeWriterEffect(aboutTitle, "Sobre Nós", 100);
+    }
+    if (contactTitle) {
+        setTimeout(() => typeWriterEffect(contactTitle, "Contatos", 100), 500);
+    }
+});
+
+  function abrirImagem(elemento) {
+    var lightbox = document.getElementById('lightbox');
+    var imagemAmpliada = document.getElementById('imagemAmpliada');
+    imagemAmpliada.src = elemento.src;
+    lightbox.style.display = 'flex';
+}
+
+function fecharLightbox() {
+    var lightbox = document.getElementById('lightbox');
+    lightbox.style.display = 'none';
+}
